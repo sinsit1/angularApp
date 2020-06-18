9920531340c344df4d1aecff3f867f776ce8f87c
sinsit <alexandra8989@gmail.com>
2020-06-18 09:52:58 +0200

    wip

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare function init_plugins();
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: 'app-login', templateUrl: './login.component.html', styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    init_plugins();
+  }
+
+  login() {
+    this.router.navigate(['/dashboard']);
   }
 
 }
